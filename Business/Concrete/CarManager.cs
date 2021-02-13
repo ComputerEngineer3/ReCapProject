41b49f229a92e8cf46c5c4f4ae293b8609e7155e
ComputerEngineer3 <aynr.fsjf@outlook.com>
2021-02-13 14:10:52 +0300

    SekizinciGunodevi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -15,9 +15,39 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.Description.Length >= 2)
+            {
+                if (car.DailyPrice > 0)
+                {
+                    _carDal.Add(car);
+                }
+                else
+                {
+                    Console.WriteLine("Araba günlük fiyatı 0'dan büyük olmalıdır.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Araba ismi minimum 2 karakter olmalıdır.");
+            }
+
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,12 +10,47 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            CarManager carManager = new CarManager(new EfCarDal());
 
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description);
             }
+
+            Console.WriteLine("---------------------------------------------------");
+
+            Console.WriteLine("");
+            Console.WriteLine(" Fiat Cars");
+            Console.WriteLine("--------------");
+            foreach (var car in carManager.GetCarsByBrandId(2))
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            Console.WriteLine("---------------------------------------------------");
+
+            Console.WriteLine("");
+            Console.WriteLine(" White Cars");
+            Console.WriteLine("--------------");
+            foreach (var car in carManager.GetCarsByColorId(1))
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine("---------------------------------------------------");
+
+
+            
+            Car newCar = new Car { BrandId=9, ColorId=5, ModelYear=2019, DailyPrice=220, Description= "Audi Q3" };
+
+            //carManager.Add(newCar);
+
+            /*Console.WriteLine("");
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }*/
+
         }
     }
 }
